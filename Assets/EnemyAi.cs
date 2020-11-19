539a56e0431905dba7d5f44e05d9b80f3b9c155d
Julian <julianemoreno34@gmail.com>
2020-11-18 20:23:01 -0600

    Added Enemy AI, visuals, and listen mode mechanic added

---
+++ Assets/EnemyAi.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAi : MonoBehaviour
+{ 
+   public NavMeshAgent agent;
+
+
+
+public LayerMask whatIsGround, whatIsPlayer;
+
+//public float health;
+
+//Patroling
+public Vector3 walkPoint;
+bool walkPointSet;
+public float walkPointRange;
+
+
+
+//States
+public float sightRange, attackRange;
+public bool playerInSightRange, playerInAttackRange;
+
+private void Awake()
+{
+   // player = GameObject.Find("PlayerObj").transform;
+    agent = GetComponent<NavMeshAgent>();
+}
+
+private void Update()
+{
+
+
+    if (!playerInSightRange && !playerInAttackRange) Patroling();
+
+}
+
+private void Patroling()
+{
+    if (!walkPointSet) SearchWalkPoint();
+
+    if (walkPointSet)
+        agent.SetDestination(walkPoint);
+
+    Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+    //Walkpoint reached
+    if (distanceToWalkPoint.magnitude < 1f)
+        walkPointSet = false;
+}
+private void SearchWalkPoint()
+{
+    //Calculate random point in range
+    float randomZ = Random.Range(-walkPointRange, walkPointRange);
+    float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+    walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+    if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        walkPointSet = true;
+}
+
+
+
+
+private void OnDrawGizmosSelected()
+{
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(transform.position, attackRange);
+    Gizmos.color = Color.yellow;
+    Gizmos.DrawWireSphere(transform.position, sightRange);
+}
+}
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,24 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class playerController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
     public float speed = 6f;
+    public  GameObject processing;
+   public bool listenMode;
+    public Slider slider;
 
+    public GameObject Obj1;
+    public GameObject Obj2;
+    public float Distance;
+    public GUIStyle style;
+    [SerializeField] GameObject zombieParticle;
+
+   // GameObject varGameObject = GameObject.FindWithTag("Enemy");
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        style.fontSize = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        Distance = Vector3.Distance(Obj1.transform.position, Obj2.transform.position);
+       // bool listenPressed = Input.GetButton("l");
+       // bool listenUnpressed = Input.GetButton(KeyCode.L);
+
+        if ( Input.GetKey("l" ) )
+        {
+         
+          // GameObject.FindWithTag("Enemy").GetComponent<Outline>().enabled = true;
+         slider.value -= 1f;
+            listenMode = true;
+          processing.SetActive(true);
+            if(Distance <13)
+            {
+                zombieParticle.SetActive(true);
+              //  particleSystem.playOnAwake;
+                GameObject.FindWithTag("Enemy").GetComponent<Outline>().enabled = true;
+            }
+            if(Distance >13)
+            {
+                zombieParticle.SetActive(false);
+                GameObject.FindWithTag("Enemy").GetComponent<Outline>().enabled = false;
+            }
+        }
+       else
+        {
+            zombieParticle.SetActive(false);
+            GameObject.FindWithTag("Enemy").GetComponent<Outline>().enabled = false;
+            slider.value += 2f;
+            listenMode = false;
+           processing.SetActive(false);
+        }
+        
+        if( slider.value == 0)
+        {
+            zombieParticle.SetActive(false);
+            GameObject.FindWithTag("Enemy").GetComponent<Outline>().enabled = false;
+            listenMode = false;
+          processing.SetActive(false);
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -35,5 +88,10 @@ public class playerController : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
         
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(50, 150, 200, 200), "Value :" + Distance, style);
     }
 }
